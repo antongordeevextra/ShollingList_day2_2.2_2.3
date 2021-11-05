4cac38cb3deceda85410651bbd5e24c35dd1ecfe
Anton Gordeev <antongordeev9505@gmail.com>
2021-11-05 18:20:00 +0300

    2.2

--- app/src/main/java/com/example/shollinglist_day2_22/MainActivity.kt
+++ app/src/main/java/com/example/shollinglist_day2_22/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.shollinglist_day2_22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.core.view.isVisible
+import com.example.shollinglist_day2_22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.fab.setOnClickListener {
+            startActivityForResult(Intent(this, SecondActivity::class.java), REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            data?.let {
+                val name1 = data.getStringExtra("item1")
+                val name2 = data.getStringExtra("item2")
+                val name3 = data.getStringExtra("item3")
+                val name4 = data.getStringExtra("item4")
+                val name5 = data.getStringExtra("item5")
+
+                val list = listOf(name1, name2, name3, name4, name5)
+
+                list.forEach {
+                    if (it != null) {
+                        when(list.indexOf(it)) {
+                            0 -> {
+                                binding.id1.text = it
+                                binding.id1.isVisible = true
+                            }
+                            1 -> {
+                                binding.id2.text = it
+                                binding.id2.isVisible = true
+                            }
+                            2 -> {
+                                binding.id3.text = it
+                                binding.id3.isVisible = true
+                            }
+                            3 -> {
+                                binding.id4.text = it
+                                binding.id4.isVisible = true
+                            }
+                            4 -> {
+                                binding.id5.text = it
+                                binding.id5.isVisible = true
+                            }
+                            else -> Unit
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/shollinglist_day2_22/SecondActivity.kt
+++ app/src/main/java/com/example/shollinglist_day2_22/SecondActivity.kt
@@ -1,11 +1,45 @@
 package com.example.shollinglist_day2_22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.view.forEach
+import com.example.shollinglist_day2_22.databinding.ActivityMainBinding
+import com.example.shollinglist_day2_22.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.root.forEach {
+            if (it is Button) it.setOnClickListener(universalButtonListener)
+        }
     }
+
+    private val universalButtonListener = View.OnClickListener {
+        when (it.id) {
+            R.id.item1 -> sendBack("item1", binding.item1.text.toString())
+            R.id.item2 -> sendBack("item2", binding.item2.text.toString())
+            R.id.item3 -> sendBack("item3", binding.item3.text.toString())
+            R.id.item4 -> sendBack("item4", binding.item4.text.toString())
+            R.id.item5 -> sendBack("item5", binding.item5.text.toString())
+            else -> Unit
+        }
+    }
+
+    private fun sendBack(key: String, value: String) {
+        val intent = Intent()
+        intent.putExtra(key, value)
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
 }
